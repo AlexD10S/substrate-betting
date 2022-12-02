abc3aa028faddb71c6d102c2897d6fa55b8918b3
Artur Gontijo <arturgontijo@gmail.com>
2022-12-02 09:52:41 -0300

    fmt and removing without_storage_info

--- src/mock.rs
+++ src/mock.rs
@@ -1,11 +1,11 @@
 use crate as pallet_betting;
-use frame_support::traits::{ConstU16,ConstU32, ConstU64, ConstU128};
+use frame_support::traits::{ConstU128, ConstU16, ConstU32, ConstU64};
 use frame_support::{parameter_types, PalletId};
 use frame_system as system;
 use sp_core::H256;
 use sp_runtime::{
-	testing::Header,
-	traits::{BlakeTwo256, IdentityLookup},
+    testing::Header,
+    traits::{BlakeTwo256, IdentityLookup},
 };
 
 type UncheckedExtrinsic = frame_system::mocking::MockUncheckedExtrinsic<Test>;
@@ -13,15 +13,15 @@ type Block = frame_system::mocking::MockBlock<Test>;
 
 // Configure a mock runtime to test the pallet.
 frame_support::construct_runtime!(
-	pub enum Test where
-		Block = Block,
-		NodeBlock = Block,
-		UncheckedExtrinsic = UncheckedExtrinsic,
-	{
-		System: frame_system,
-		Betting: pallet_betting,
-		Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
-	}
+    pub enum Test where
+        Block = Block,
+        NodeBlock = Block,
+        UncheckedExtrinsic = UncheckedExtrinsic,
+    {
+        System: frame_system,
+        Betting: pallet_betting,
+        Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
+    }
 
 );
 
@@ -38,41 +38,42 @@ impl pallet_balances::Config for Test {
 }
 
 impl system::Config for Test {
-	type BaseCallFilter = frame_support::traits::Everything;
-	type BlockWeights = ();
-	type BlockLength = ();
-	type DbWeight = ();
-	type RuntimeOrigin = RuntimeOrigin;
-	type RuntimeCall = RuntimeCall;
-	type Index = u64;
-	type BlockNumber = u64;
-	type Hash = H256;
-	type Hashing = BlakeTwo256;
-	type AccountId = u64;
-	type Lookup = IdentityLookup<Self::AccountId>;
-	type Header = Header;
-	type RuntimeEvent = RuntimeEvent;
-	type BlockHashCount = ConstU64<250>;
-	type Version = ();
-	type PalletInfo = PalletInfo;
-	type AccountData = pallet_balances::AccountData<u128>;
-	type OnNewAccount = ();
-	type OnKilledAccount = ();
-	type SystemWeightInfo = ();
-	type SS58Prefix = ConstU16<42>;
-	type OnSetCode = ();
-	type MaxConsumers = frame_support::traits::ConstU32<16>;
+    type BaseCallFilter = frame_support::traits::Everything;
+    type BlockWeights = ();
+    type BlockLength = ();
+    type DbWeight = ();
+    type RuntimeOrigin = RuntimeOrigin;
+    type RuntimeCall = RuntimeCall;
+    type Index = u64;
+    type BlockNumber = u64;
+    type Hash = H256;
+    type Hashing = BlakeTwo256;
+    type AccountId = u64;
+    type Lookup = IdentityLookup<Self::AccountId>;
+    type Header = Header;
+    type RuntimeEvent = RuntimeEvent;
+    type BlockHashCount = ConstU64<250>;
+    type Version = ();
+    type PalletInfo = PalletInfo;
+    type AccountData = pallet_balances::AccountData<u128>;
+    type OnNewAccount = ();
+    type OnKilledAccount = ();
+    type SystemWeightInfo = ();
+    type SS58Prefix = ConstU16<42>;
+    type OnSetCode = ();
+    type MaxConsumers = frame_support::traits::ConstU32<16>;
 }
 
 parameter_types! {
-	pub const BettingPalletId: PalletId = PalletId(*b"bet_mock");
+    pub const BettingPalletId: PalletId = PalletId(*b"bet_mock");
 }
 
 impl pallet_betting::Config for Test {
-	type RuntimeEvent = RuntimeEvent;
-	type PalletId = BettingPalletId;
-	type Currency = Balances;
-	type MaxBetsPerMatch = ConstU32<3>;
+    type PalletId = BettingPalletId;
+    type Currency = Balances;
+    type RuntimeEvent = RuntimeEvent;
+    type MaxTeamNameLength = ConstU32<64>;
+    type MaxBetsPerMatch = ConstU32<3>;
 }
 
 pub(crate) const ACCOUNT_A: u64 = 0;
@@ -83,7 +84,7 @@ pub(crate) const ACCOUNT_E: u64 = 4;
 pub(crate) const INIT_BALANCE: u128 = 1_000_000_000_000_000;
 // Build genesis storage according to the mock runtime.
 pub fn new_test_ext() -> sp_io::TestExternalities {
-	let mut storage = frame_system::GenesisConfig::default()
+    let mut storage = frame_system::GenesisConfig::default()
         .build_storage::<Test>()
         .unwrap();
 
@@ -92,14 +93,14 @@ pub fn new_test_ext() -> sp_io::TestExternalities {
             (ACCOUNT_A, INIT_BALANCE),
             (ACCOUNT_B, INIT_BALANCE),
             (ACCOUNT_C, INIT_BALANCE),
-			(ACCOUNT_D, INIT_BALANCE),
-			(ACCOUNT_E, INIT_BALANCE),
+            (ACCOUNT_D, INIT_BALANCE),
+            (ACCOUNT_E, INIT_BALANCE),
         ],
     }
     .assimilate_storage(&mut storage)
     .unwrap();
-	
-	let mut test_ext: sp_io::TestExternalities = storage.into();
+
+    let mut test_ext: sp_io::TestExternalities = storage.into();
     test_ext.execute_with(|| System::set_block_number(1));
     test_ext
 }
