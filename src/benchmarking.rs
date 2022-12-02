d1224b7f24d7b134d586e2550326a205b708abe0
Artur Gontijo <arturgontijo@gmail.com>
2022-12-02 15:29:17 -0300

    [WIP] Adding benchmarking...

--- src/benchmarking.rs
+++ src/benchmarking.rs
@@ -4,16 +4,86 @@ use super::*;
 
 #[allow(unused)]
 use crate::Pallet as Betting;
-use frame_benchmarking::{benchmarks, whitelisted_caller};
+use frame_benchmarking::{account, benchmarks, whitelisted_caller};
 use frame_system::RawOrigin;
+use sp_runtime::traits::One;
+
+fn create_match<T: Config>(result: Option<MatchResult>) -> T::AccountId {
+    let caller: T::AccountId = account("creator", 0, 0);
+    let start = frame_system::Pallet::<T>::block_number();
+    let length = T::BlockNumber::one();
+
+    let betting_match = Match {
+        start,
+        length,
+        team1: <BoundedVec<_, T::MaxTeamNameLength>>::try_from("team1".as_bytes().to_vec())
+            .unwrap(),
+        team2: <BoundedVec<_, T::MaxTeamNameLength>>::try_from("team2".as_bytes().to_vec())
+            .unwrap(),
+        result,
+        bets: Default::default(),
+    };
+
+    let match_hash = Betting::<T>::get_match_hash(betting_match.clone());
+
+    <MatchHashes<T>>::insert(&match_hash, caller.clone());
+    <Matches<T>>::insert(&caller, betting_match);
+
+    caller
+}
+
+fn add_bet<T: Config>(user: &'static str, match_id: AccountIdOf<T>, a: u32, r: MatchResult) {
+    let caller = account(user, 0, 0);
+    T::Currency::make_free_balance_be(&caller, BalanceOf::<T>::from(10u32));
+    let origin = <T::RuntimeOrigin>::from(RawOrigin::Signed(caller));
+    let _ = Betting::<T>::bet(origin, match_id, a.into(), r);
+}
 
 benchmarks! {
-    do_something {
-        let s in 0 .. 100;
+    create_match_to_bet {
         let caller: T::AccountId = whitelisted_caller();
-    }: _(RawOrigin::Signed(caller), s)
+        let team1 = "team1".as_bytes().to_vec();
+        let team2 = "team2".as_bytes().to_vec();
+        let start = frame_system::Pallet::<T>::block_number();
+        let length = T::BlockNumber::one();
+    }: _(RawOrigin::Signed(caller.clone()), team1, team2, start, length)
     verify {
-        assert_eq!(Something::<T>::get(), Some(s));
+        assert!(Matches::<T>::contains_key(&caller));
+    }
+
+    bet {
+        let match_id = create_match::<T>(None);
+        let caller: T::AccountId = whitelisted_caller();
+        T::Currency::make_free_balance_be(&caller, BalanceOf::<T>::from(10u32));
+        let amount = BalanceOf::<T>::from(5u32);
+        let result = MatchResult::Draw;
+    }: _(RawOrigin::Signed(caller.clone()), match_id.clone(), amount, result)
+    verify {
+        let m = Matches::<T>::get(&match_id).unwrap();
+        assert_eq!(m.bets.len(), 1);
+    }
+
+    set_result {
+        let match_id = create_match::<T>(None);
+        let result = MatchResult::Team1Victory;
+    }: _(RawOrigin::Root, match_id.clone(), result)
+    verify {
+        let m = Matches::<T>::get(&match_id).unwrap();
+        assert_eq!(m.result, Some(MatchResult::Team1Victory));
+    }
+
+    distribute_winnings {
+        let match_id = create_match::<T>(Some(MatchResult::Team1Victory));
+        frame_system::Pallet::<T>::set_block_number(3u32.into());
+        let result = MatchResult::Team1Victory;
+        add_bet::<T>("user1", match_id.clone(), 1, MatchResult::Team1Victory);
+        add_bet::<T>("user2", match_id.clone(), 2, MatchResult::Team2Victory);
+        add_bet::<T>("user3", match_id.clone(), 3, MatchResult::Draw);
+        add_bet::<T>("user4", match_id.clone(), 4, MatchResult::Draw);
+        add_bet::<T>("user5", match_id.clone(), 5, MatchResult::Team1Victory);
+    }: _(RawOrigin::Signed(match_id.clone()))
+    verify {
+        assert_eq!(Matches::<T>::contains_key(&match_id), false);
     }
 
     impl_benchmark_test_suite!(Betting, crate::mock::new_test_ext(), crate::mock::Test);
