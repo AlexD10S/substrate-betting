1b5f9fcfbdad1cc1ceaa0f8f5094936a250173a5
Artur Gontijo <arturgontijo@gmail.com>
2022-12-06 12:48:23 +0100

    [RPC] Using RpcResult.

--- rpc/runtime-api/src/lib.rs
+++ rpc/runtime-api/src/lib.rs
@@ -1,14 +1,15 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 use codec::Codec;
 use sp_runtime::traits::MaybeDisplay;
+use jsonrpsee::code::RpcResult;
 
 // Here we declare the runtime API. It is implemented it the `impl` block in
 // runtime file (the `runtime/src/lib.rs`)
 sp_api::decl_runtime_apis! {
-    pub trait BettingApi <AccountId, Match> where
+    pub trait BettingApi<AccountId, Match> where
         AccountId: Codec + MaybeDisplay,
         Match: Codec + MaybeDisplay,
     {
-        fn get_match(match_id: AccountId) -> Match;
+        fn get_match(match_id: AccountId) -> RpcResult<Match>;
     }
 }
